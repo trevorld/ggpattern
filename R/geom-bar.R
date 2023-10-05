c675a5e386987ecf67855a3033b3d0626973f3e1
Trevor L Davis <trevor.l.davis@gmail.com>
2023-10-05 11:23:16 -0700

    feat: 'geom_bin_2d_pattern()' as 'geom_bin2d_pattern()' alias
    * `geom_bin_2d_pattern()` is now an alias for `geom_bin2d_pattern()`.
      This matches `{ggplot2}` which has both `geom_bin_2d()` and `geom_bin2d()`.
    * We also place `geom_histogram_pattern()` in `geom-histogram.R`
      to better match the file structure in `{ggplot2}` to make it easier
      to scan for the differences we maintain.

--- R/geom-bar.R
+++ R/geom-bar.R
@@ -143,23 +143,6 @@ geom_bar_pattern <- function(mapping = NULL, data = NULL,
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' @rdname geom-docs
-#' @export
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-geom_histogram_pattern <- function (mapping = NULL, data = NULL,
-                                    stat = "bin", position = "stack", ...,
-                                    binwidth = NULL, bins = NULL, na.rm = FALSE,
-                                    orientation = NA,
-                                    show.legend = NA, inherit.aes = TRUE)
-{
-    layer(data = data, mapping = mapping, stat = stat, geom = GeomBarPattern,
-          position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-          params = list(binwidth = binwidth, bins = bins, na.rm = na.rm,
-                        orientation = orientation, pad = FALSE, ...))
-}
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' Geom ggproto objects
 #'
 #' Geom ggproto objects that could be extended to create a new geom.
